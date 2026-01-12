cc455b21324daea2feedad230cc201c86afc34d2
rtandico <100163401+rtandico@users.noreply.github.com>
2026-01-12 14:28:10 -0600

    Add files via upload

--- components/ratgdo/switch/ratgdo_switch.cpp
+++ components/ratgdo/switch/ratgdo_switch.cpp
@@ -1,1 +1,47 @@
+#include "ratgdo_switch.h"
+#include "../ratgdo_state.h"
+#include "esphome/core/log.h"
 
+namespace esphome {
+namespace ratgdo {
+
+    static const char* const TAG = "ratgdo.switch";
+
+    void RATGDOSwitch::dump_config()
+    {
+        LOG_SWITCH("", "RATGDO Switch", this);
+        if (this->switch_type_ == SwitchType::RATGDO_LEARN) {
+            ESP_LOGCONFIG(TAG, "  Type: Learn");
+        }
+    }
+
+    void RATGDOSwitch::setup()
+    {
+        if (this->switch_type_ == SwitchType::RATGDO_LEARN) {
+            this->parent_->subscribe_learn_state([=](LearnState state) {
+                this->publish_state(state == LearnState::ACTIVE);
+            });
+        } else if (this->switch_type_ == SwitchType::RATGDO_LED) {
+            this->pin_->setup();
+            this->parent_->subscribe_vehicle_arriving_state([=](VehicleArrivingState state) {
+                this->write_state(state == VehicleArrivingState::YES);
+            });
+        }
+    }
+
+    void RATGDOSwitch::write_state(bool state)
+    {
+        if (this->switch_type_ == SwitchType::RATGDO_LEARN) {
+            if (state) {
+                this->parent_->activate_learn();
+            } else {
+                this->parent_->inactivate_learn();
+            }
+        } else if (this->switch_type_ == SwitchType::RATGDO_LED) {
+            this->pin_->digital_write(state);
+            this->publish_state(state);
+        }
+    }
+
+} // namespace ratgdo
+} // namespace esphome
---
+++ components/ratgdo/switch/ratgdo_switch.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "../ratgdo.h"
+#include "../ratgdo_state.h"
+#include "esphome/components/switch/switch.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace ratgdo {
+
+    enum SwitchType {
+        RATGDO_LEARN,
+        RATGDO_LED
+    };
+
+    class RATGDOSwitch : public switch_::Switch, public RATGDOClient, public Component {
+    public:
+        void dump_config() override;
+        void setup() override;
+        void set_switch_type(SwitchType switch_type_) { this->switch_type_ = switch_type_; }
+
+        void write_state(bool state) override;
+        void set_pin(GPIOPin* pin) { pin_ = pin; }
+
+    protected:
+        SwitchType switch_type_;
+        GPIOPin* pin_;
+    };
+
+} // namespace ratgdo
+} // namespace esphome
